56dc0f43b2e0f0e2c0d5e8bfe8cea5de849c9c45
Akashreghukumar <47936922+Akashreghukumar@users.noreply.github.com>
2022-01-07 14:54:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express()
+
+
+
+const mongo=require('./shared/connection');
+
+const cors=require('cors')
+// const user=require('./routes/movies')
+const registerRouter=require('./routes/register')
+const userRouter=require('./routes/user')
+
+const authorize=require('./module/authorize')
+
+const movieRouter=require('./routes/movie')
+
+app.use(express.json())
+// console.log(mongo)
+ mongo.connect()
+
+ app.use(cors())
+//  app.use('/user',user)
+ app.use('/register',registerRouter)
+
+ app.use(authorize.Authorize)
+//  app.use(authorize.isadmin)
+app.use('/user',userRouter)
+app.use('/movie',movieRouter)
+app.listen(3001) 
